b10b062aa4d3e24871ee5f15becc4e3bb1a63b4a
Zulkar Nine <zulkarnine43@gmail.com>
2021-06-19 00:25:44 +0600

    laravel API working front_end as React

--- src/UpdateProduct.js
+++ src/UpdateProduct.js
@@ -9,6 +9,8 @@ constructor(props) {
     super(props);
     this.state = {
       name: '',
+       price: '',
+        description: '',
     };
 
   }
@@ -22,7 +24,9 @@ componentDidMount(){
        axios.get('http://localhost/apiLearn/public/api/productById/'+id)
           .then((response)=> {
             // handle success
-            this.setState({name:response.data.name});
+            this.setState({name:response.data.name,price:response.data.price,
+              description:response.data.description
+            });
          // console.log(response);
            
           })
@@ -32,10 +36,42 @@ componentDidMount(){
           })
 }
 
+formSubmit=(e)=>{
+e.preventDefault();
+const id=this.props.match.params.id;
+
+const names = {name: this.state.name, price: this.state.price, description: this.state.description};
+
+ axios.post('http://localhost/apiLearn/public/api/productUpdate/'+id,names)
+          .then((response)=> {
+            alert("success");
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+          })
+}
+
   render() {
     return (
-
-        <h2>{this.state.name}</h2>
+         <>
+            <Header />
+            <form onSubmit={this.formSubmit}>
+            <div class="col-md-6 offset-md-3" >
+                <h1>Update Product</h1>
+                <br />
+                <input type="text" value={this.state.name} onChange={(e) =>{ this.setState({name:e.target.value})} } className='form-control' />
+                <br />
+                <input type="file" className='form-control' />
+                <br />
+                <input type="text" value={this.state.price}  onChange={(e) =>{ this.setState({price:e.target.value})} } className='form-control' />
+                <br />
+                <input type="text" value={this.state.description} onChange={(e) =>{ this.setState({description:e.target.value})} }  className='form-control' />
+                <br />
+                <button type="submit" className="btn btn-primary">Add Product</button>
+            </div>
+            </form>
+        </>
 
         );
   }
